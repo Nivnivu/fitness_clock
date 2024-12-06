2973cc3b6ffb06d0fa138e67deac51b662f88c49
NivBarsh <nivbarsh@gmail.com>
2024-12-06 09:28:19 +0200

    fixing clock

--- app/components/Clock.js
+++ app/components/Clock.js
@@ -25,18 +25,17 @@ export default function Clock() {
   return (
     <div className="flex flex-col items-center justify-center gap-8 my-16">
       {/* Logo */}
-      <div className="w-[300px] lg:w-[400px]">
+      <div className="relative w-[200px] lg:w-[400px] h-[100px] lg:h-[200px] overflow-hidden">
         <Image
           src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/logo-W-hPRb194aEtcDVdqyo30RxrqHG8TtEN.svg"
           alt="Fitness Factory Logo"
-          width={400}
-          height={400}
-          className="w-full h-auto"
+          fill
+          className="object-contain"
         />
       </div>
       {/* Clock */}
-      <div className="flex items-center justify-center bg-gray-900 border-4 border-gray-100 rounded-3xl shadow-xl px-6 py-4">
-        <span className="text-white font-mono text-[4rem] sm:text-[6rem] md:text-[8rem] lg:text-[10rem] xl:text-[12rem] ">
+      <div className="flex items-center justify-center bg-black border-4 border-gray-100 rounded-3xl shadow-xl px-6 py-4">
+        <span className="text-white font-mono text-[4rem] sm:text-[6rem] md:text-[8rem] lg:text-[10rem] xl:text-[12rem]">
           {formattedTime}
         </span>
       </div>
