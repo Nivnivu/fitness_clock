0dc92bfe6e106b684262f43b1369ebad487cd334
NivBarsh <nivbarsh@gmail.com>
2024-12-06 09:12:53 +0200

    screen

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Clock from './components/Clock'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-gray-800 to-black flex items-center justify-center">
+    <main className="min-h-[90%] bg-gradient-to-b from-gray-800 to-black flex items-center justify-center">
       <Clock />
     </main>
   )
