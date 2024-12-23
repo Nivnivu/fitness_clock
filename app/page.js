1c3afc0cf6cafbb3a4ac57bb23e8b1f391df29e8
Niv Yaakov Barchechet <Nivbarsh@gmail.com>
2024-12-23 21:20:24 +0200

    darkcscheme

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Clock from './components/Clock'
 
 export default function Home() {
   return (
-    <main className="min-h-screen max-h-screen bg-black flex items-center justify-center">
+    <main className="min-h-screen max-h-screen dark:bg-black bg-black flex items-center justify-center">
       <Clock />
     </main>
   )
