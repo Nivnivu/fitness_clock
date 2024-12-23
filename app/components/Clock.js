ea891e8e80e7f4478b2e19534a5828657031e285
Niv Yaakov Barchechet <Nivbarsh@gmail.com>
2024-12-23 22:50:44 +0200

    Update Clock.js

--- app/components/Clock.js
+++ app/components/Clock.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 export default function Clock() {
   const [time, setTime] = useState(null);
@@ -26,18 +27,28 @@ export default function Clock() {
   }).format(time);
 
   return (
-    <div className="clock-container">
+    <div className="flex flex-col items-center justify-center gap-8 my-16">
       {/* Logo */}
-      <div className="logo-container">
-        <img
+      <div className="relative w-[200px] lg:w-[500px] h-[100px] lg:h-[300px] overflow-hidden text-center">
+        <Image
           src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/logo-W-hPRb194aEtcDVdqyo30RxrqHG8TtEN.svg"
           alt="Fitness Factory Logo"
-          className="logo"
+          fill
+          className="object-contain"
         />
       </div>
       {/* Clock */}
-      <div className="clock">
-        <span className="clock-time">{formattedTime}</span>
+      <div className="flex items-center justify-center dark:bg-black bg-black px-6 py-4">
+        <span
+          className="text-white font-mono text-[4rem] sm:text-[6rem] md:text-[8rem] lg:text-[10rem] xl:text-[12rem] text-center"
+          style={{
+            border: 'none',
+            outline: 'none',
+            boxShadow: 'none',
+          }}
+        >
+          {formattedTime}
+        </span>
       </div>
     </div>
   );
