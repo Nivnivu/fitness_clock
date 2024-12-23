1facde9977554550042588d2e82130e5ad08f6a9
Niv Yaakov Barchechet <Nivbarsh@gmail.com>
2024-12-23 22:55:03 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import dynamic from 'next/dynamic';
 
-import Clock from './components/Clock'
+// Dynamically import the Clock component with SSR disabled
+const Clock = dynamic(() => import('./components/Clock'), { ssr: false });
 
 export default function Home() {
   return (
     <main className="min-h-screen max-h-screen dark:bg-black bg-black flex items-center justify-center">
       <Clock />
     </main>
-  )
+  );
 }
